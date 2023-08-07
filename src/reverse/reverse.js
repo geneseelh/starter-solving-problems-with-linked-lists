a2fcd366d8596ddd0d989182a3d0f46fb64c09a2
Genesee Harmon <geneseelh@gmail.com>
2023-08-06 19:17:36 -0700

    initial commit

--- src/reverse/reverse.js
+++ src/reverse/reverse.js
@@ -9,7 +9,20 @@ const LinkedList = require("../lib/linkedList");
  * @returns {LinkedList}
  * A linked list in reverse order
  */
-function reverseIterative(list) {}
+function reverseIterative(list) {
+    const reversedList = new LinkedList();
+    const node = list.head;
+
+    //while node is not null
+    while (node) {
+        // insert node value at head of reversedList
+        reversedList.insertAtHead(node.value);
+        // move the node pointer to the next node
+        node = node.next;
+    }
+    // return the new reversedList
+    return reversedList;
+}
 
 /**
  * Recursively reverse a linked list
